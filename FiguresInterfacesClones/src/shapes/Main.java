5aef67cf724ed835d85c67aa9131741c1765ef77
Stetson Dan <dplante@stetson.edu>
2026-02-06 15:16:24 -0500

    Adding cloning and copy constructors

--- FiguresInterfacesClones/src/shapes/Main.java
+++ FiguresInterfacesClones/src/shapes/Main.java
@@ -14,17 +14,28 @@ public class Main {
 		square1 = new Square(10.0, Color.BLUE, new Point(4,5));
 		square1.setSideLength(6.0);
 
-		Square square2 = new Square(5.0, Color.CYAN, new Point(6, 7));
+		Point point = new Point(6, 7);
+		Square square2 = new Square(5.0, Color.CYAN, new Point(point));
 		
 		/*
 		 * setting "square3 = square2" ONLY sets the memory location
 		 * of square3 to that of square2; this is called an
 		 * "aliasing" problem.
+		 * 
+		 * Because the "clone()" method returns an Object (not a Square)
+		 * we must "cast" the returned object to its proper type, 
+		 * namely a Square here.
 		 */
-		Square square3 = square2;
-		/* XXXXXXXXXXXXXXX I Stopped here!!!! */
+		Square square3 = (Square)square2.clone();
 		square3.setSideLength(7.0);
 		square3.setColor(Color.GREEN);
+		square3.moveTo(111, 222);
+		
+		/*
+		 * Use the "copy constructor" to create a new object
+		 */
+		Square square4 = Square.copyOf(square3);
+		square4.moveTo(33, 44);
 		/*
 		 *  "Override" the "toString()" method in the Square class to 
 		 *  clean up the printing below
@@ -33,7 +44,7 @@ public class Main {
 		System.out.println(square1);
 		System.out.println(square2);
 		System.out.println(square3);
-
+		System.out.println(square4);
 		
 	}
 
--- FiguresInterfacesClones/src/shapes/Square.java
+++ FiguresInterfacesClones/src/shapes/Square.java
@@ -11,6 +11,17 @@ public class Square extends Shape implements Location {
 	private double sideLength;
 	private Point point;
 	
+	/*
+	 * "this" means call this class constructor with 3 arguments, which
+	 * we have below
+	 * 
+	 * "super" means call the parent class constructor with its arguments
+	 * (in this case 1, the color)
+	 */
+	public Square() {
+		this(1.0, Color.WHITE, new Point(0,0));
+	}
+	
 	public Square(double sideLength, Color color, Point point) {
 		// calling superclass constructor
 		super(color);
@@ -26,6 +37,28 @@ public class Square extends Shape implements Location {
 		return this.getClass().getSimpleName() + " sideLength = " + sideLength 
 				+ "  area = " + area + super.toString()
 				+ "  location = (" + point.x + ", " + point.y + ")";
+	}
+	
+	/*
+	 * cloning is okay but copy constructor is better
+	 */
+	@Override
+	public Object clone() {
+		return new Square(sideLength, this.getColor(), new Point(point));
+	}
+	
+	/*
+	 * The preferred way to create a new object from this class is to use
+	 * a "copy constructor"
+	 * 
+	 * "static" does not mean you cannot change it (that is what "final"
+	 * means; it means it belongs to the class, not objects created from the
+	 * class; therefore it cannot call properties (or attributes or fields)
+	 * from within the static method.  We need to pass in a square.
+	 */
+	
+	public static Square copyOf(Square square) {
+		return new Square(square.sideLength, square.getColor(), new Point(square.locate()));
 	}
 	
 	public double getSideLength() {
