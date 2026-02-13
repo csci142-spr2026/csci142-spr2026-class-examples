dd7e08f3ce538c128c2bcb2becf8f71c628919bc
Stetson Dan <dplante@stetson.edu>
2026-02-12 21:26:01 -0500

    Added Locks stubbed out files for the project

--- FiguresPolymorphism/src/shapes/Main.java
+++ FiguresPolymorphism/src/shapes/Main.java
@@ -2,35 +2,54 @@ package shapes;
 
 import java.awt.Color;
 import java.awt.Point;
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
 		/*
-		 * BEGIN HERE IN MAIN WITH POLYMORPHISM!!!
+		 * In the line shapes[0] = new Square();
+		 * this line uses the Square class constructor to create a new Square object,
+		 * and then assigns it to the first element of the shapes array, 
+		 * which is of type Shape.
+		 * 
+		 * Importantly, this line "instantiates" an "instance" of the Square class, 
+		 * which is a concrete class that extends the abstract Shape class.
 		 */
+		Shape[] shapes = new Shape[6];
+		shapes[0] = new Square();
+		shapes[1] = new Rectangle();
+		shapes[2] = new Rectangle();
+		shapes[3] = new Square();
+		shapes[4] = new Rectangle();
+		shapes[5] = new Square();
+		Random random = new Random();
 		
-		Square square1;
+		for (Shape shape : shapes) {
+			shape.setColor(Color.BLUE);
+			//shape.setSideLength(2.0); // compile-time error: Shape does not have setSideLength method
+			
+			if (shape instanceof Square) {
+				Square square = (Square) shape;
+				square.setSideLength(2.0);
+			} else if (shape instanceof Rectangle) {
+				Rectangle rectangle = (Rectangle) shape;
+				rectangle.setHeight(3.0);
+				rectangle.setPoint(new Point(1, random.nextInt(10)));
+			}
+			
+			System.out.println(shape);
+		}
 		
-		square1 = new Square(Square.UNIT_LENGTH, Color.BLUE, new Point(4,5));
-		square1.setSideLength(6.0);
-
-		Point point = new Point(6, 7);
-		Square square2 = new Square(5.0, Color.CYAN, new Point(point));
-		
-		Square square3 = (Square)square2.clone();
-		square3.setSideLength(7.0);
-		square3.setColor(Color.GREEN);
-		square3.moveTo(111, 222);
-		
-		Square square4 = Square.copyOf(square3);
-		square4.moveTo(33, 44);
-		
-		System.out.println(square1);
-		System.out.println(square2);
-		System.out.println(square3);
-		System.out.println(square4);
-		
+		/*
+		 * Polymorphism doesn't need to be for an array of shapes.  
+		 * We can have a single shape variable that can refer to any shape object.
+		 */
+		Shape rect = new Rectangle(2.0, 3.0, Color.RED, new Point(5, 5));
+		System.out.println(rect);
+		//rect.setWidth(4.0); // compile-time error: Shape does not have setWidth method
+		((Rectangle) rect).setWidth(4.0); // downcasting to Rectangle to call setWidth method
 	}
 
 }
--- FiguresPolymorphism/src/shapes/Rectangle.java
+++ FiguresPolymorphism/src/shapes/Rectangle.java
@@ -6,12 +6,22 @@ import java.awt.Point;
 public class Rectangle extends Shape implements Location {
 	private double height, width;
 	private Point point;
+	
+	public Rectangle() {
+		this(1.0, 1.0, Color.WHITE, new Point(0, 0));
+	}
 
 	public Rectangle(double height, double width, Color color, Point point) {
 		super(color);
 		this.height = height;
 		this.width = width;
 		this.point = new Point(point);
+	}
+	
+	public String toString() {
+		return this.getClass().getSimpleName() + ": width = " + width
+				+ "  height = " + height + "  area = " + area + super.toString()
+				+ "  location = (" + point.x + ", " + point.y + ")";
 	}
 
 	@Override
@@ -33,6 +43,17 @@ public class Rectangle extends Shape implements Location {
 
 	public double getHeight() {
 		return height;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		if (width <= 0) {
+			throw new IllegalArgumentException("Width must be positive");
+		}
+		this.width = width;
 	}
 
 	public void setHeight(double height) {
